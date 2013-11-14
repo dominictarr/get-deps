448493315129cf995c2c5cb8b9d3c5cbf3677765
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-14 12:28:29 +0800

    handle package.json with out deps

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ var package = require('package')
 var join = require('path').join
 
 function merge (a, b) {
-  var c = a || {}
+  a = a || {}
+  b = b || {}
   for(var k in b) {
-    c[k] = b[k]
+    a[k] = b[k]
   }
-  return c
+  return a
 }
 
 module.exports = function (start, opts) {
   start = start || process.cwd()
   var p = package({filename: join(start, 'package.json')})
   var m = !opts || opts.dev !== false
-        ? merge(p.devDependencies, p.dependencies)
+        ? merge(merge({}, p.dependencies), p.devDependencies)
         : p.dependencies
   var a = []
   for(var k in m) a.push(k + '@' + m[k])
